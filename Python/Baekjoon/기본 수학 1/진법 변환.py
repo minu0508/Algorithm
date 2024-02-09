1e038d6c1995c3191b51eb276f8c877866d4d988
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-02-09 23:50:13 +0900

    Update 진법 변환.py

--- Python/Baekjoon/기본 수학 1/진법 변환.py
+++ Python/Baekjoon/기본 수학 1/진법 변환.py
@@ -5,14 +5,11 @@ numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 answer = 0
 
 firstInputList = list(reversed(inputText[0]))
-print(firstInputList)
 
 for i in range(len(firstInputList)-1, -1, -1):
     if (numberList.count(firstInputList[i]) == 1):
         answer += int(firstInputList[i]) * (int(inputText[1]) ** i)
-        print(answer)
     else:
         answer += (ord(firstInputList[i]) - 55) * (int(inputText[1]) ** i)
-    # answer += (ord(inputAlphaAndNumber[0][i]) - 55) * int(inputAlphaAndNumber[1])**i
     
 print(answer)
