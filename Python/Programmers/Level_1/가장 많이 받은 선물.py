5e5ddceaf87b75275eef86884862c2f1a3097b23
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-04-05 11:31:17 +0900

    Add files via upload

---
+++ Python/Programmers/Level_1/가장 많이 받은 선물.py
@@ -0,0 +1,30 @@
+def solution(friends, gifts):
+    answer = [0] * len(friends)
+    givaAndTake = {i: [0] * len(friends) for i in friends}
+    futuresIndex = {i: 0 for i in friends}
+    maxGift = [0] * len(friends)
+    
+    for i in gifts:
+        i = i.split(" ")
+        givaAndTake[i[0]][friends.index(i[1])] += 1
+        futuresIndex[i[0]] += 1
+        futuresIndex[i[1]] -= 1
+    
+    for j in friends:
+        for k in range(friends.index(j) + 1, len(friends), 1):
+            if (j == friends[k]):
+                pass
+            else:
+                if (givaAndTake[j][k] > givaAndTake[friends[k]][friends.index(j)]):
+                    answer[friends.index(j)] += 1
+                elif (givaAndTake[j][k] < givaAndTake[friends[k]][friends.index(j)]):
+                    answer[friends.index(friends[k])] += 1
+                elif (givaAndTake[j][k] == givaAndTake[friends[k]][friends.index(j)]):
+                    if (futuresIndex[j] > futuresIndex[friends[k]]):
+                        answer[friends.index(j)] += 1
+                    elif (futuresIndex[j] < futuresIndex[friends[k]]):
+                        answer[friends.index(friends[k])] += 1
+                    else:
+                        pass
+    answer = max(answer)
+    return answer
