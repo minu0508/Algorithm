16156d86bfa7b87a5d5e67c5a1e561c46e519e9a
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-04-09 13:33:45 +0900

    Update OX퀴즈.py

--- Python/Programmers/Level_0/OX퀴즈.py
+++ Python/Programmers/Level_0/OX퀴즈.py
@@ -21,3 +21,20 @@ def solution(quiz):
                 answer.append("X")
 
     return answer
+
+# 24-04-09
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        i = i.split(" ")
+        if (i.count("-") == 1):
+            if (int(i[0]) - int(i[2]) == int(i[4])):
+                answer.append("O")
+            else:
+                answer.append("X")
+        else:
+            if (int(i[0]) + int(i[2]) == int(i[4])):
+                answer.append("O")
+            else:
+                answer.append("X")
+    return answer
