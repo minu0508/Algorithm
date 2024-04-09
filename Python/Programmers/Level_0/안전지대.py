a7e19439523ce589c53dc1310c62f2af7ac70462
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-04-09 17:58:39 +0900

    Update 안전지대.py

--- Python/Programmers/Level_0/안전지대.py
+++ Python/Programmers/Level_0/안전지대.py
@@ -14,3 +14,25 @@ def solution(board):
     answer = len(board[board==0])
     
     return answer
+
+#24-04-09
+import numpy as np
+
+def solution(board):
+    newBoard, answerBoard = [], []
+    
+    for i in board:
+        i = [0] + i + [0]
+        newBoard.append(i)
+    newBoard = ([[0] * len(newBoard[0])]) + newBoard + ([[0] * len(newBoard[0])])
+    newBoard = np.array(newBoard)
+    
+    x, y = np.where(newBoard==1)
+    for x, y in zip(x, y):
+        newBoard[x-1:x+2, y-1:y+2] = 1
+    
+    for i in range(1, len(newBoard)-1, 1):
+        answerBoard.append(newBoard[i][1:len(newBoard)-1])
+    answerBoard = np.array(answerBoard)
+    
+    return len(answerBoard[answerBoard==0])
