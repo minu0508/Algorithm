c7107e79d3e9f92a2835eaedb88373eb0f2665c5
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-04-09 15:07:43 +0900

    Update 평행.py

--- Python/Programmers/Level_0/평행.py
+++ Python/Programmers/Level_0/평행.py
@@ -11,3 +11,16 @@ def solution(dots):
             return 1
     else:
         return(0)
+
+# 24-04-09
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    
+    if ((y1 - y2) / (x1 - x2) == (y3 - y4) / (x3 - x4)):
+        return 1
+    if ((y1 - y3) / (x1 - x3) == (y2 - y4) / (x2 - x4)):
+        return 1
+    if ((y1 - y4) / (x1 - x4) == (y3 - y2) / (x3 - x2)):
+        return 1
+
+    return 0
